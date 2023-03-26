6dd944d0773ac29bdf6cade4b7c5bbeb8ea224a0
schreiberben <bschreiber91@gmail.com>
2023-03-26 11:09:15 +0300

    Add themes to kitty

--- kitty/install.sh
+++ kitty/install.sh
@@ -1,2 +1,9 @@
 brew install --cask kitty
 ln -fs ~/.dotfiles/kitty/kitty.conf ~/.config/kitty/kitty.conf
+
+
+for theme_file in ~/.dotfiles/kitty/themes; do
+    ln -sf ~/.dotfiles/kitty/themes/$theme_file ~/.config/kitty/$theme_file
+done
+
+kitty +kitten themes --reload-in=all Rosé Pine Moon
