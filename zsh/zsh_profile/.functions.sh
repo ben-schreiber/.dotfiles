460b7bb155d14d00b76997d051205e82b1c9a116
Ben Schreiber <bschreiber91@gmail.com>
2024-07-13 21:39:15 +0300

    July 13, 2024

--- zsh/zsh_profile/.functions.sh
+++ zsh/zsh_profile/.functions.sh
@@ -6,7 +6,21 @@ get_default_branch() {
     git rev-parse --abbrev-ref origin/HEAD | cut -c8- | tr -d '\n'
 }
 
-function download_logs {
+download_logs_dev() {
+     export VIRTUAL_CLUSTER_ID_DEV=ckrfa0ju0pnm3z72ljv1p1gf1
+     LOG_BASE_URI="s3://pgy-luigi-emr-dev/logs/eks/joblogs/$1"
+     PODS_LOG_LOCATION="$LOG_BASE_URI/$VIRTUAL_CLUSTER_ID_DEV/jobs/$2/"
+     DRIVER_POD="spark-$2-driver"
+     DRIVER_LOG_LOCATION="${PODS_LOG_LOCATION}containers/spark-$2/${DRIVER_POD}/"
+     TARGET_FOLDER="/tmp/spark_logs/$2"
+     mkdir -p $TARGET_FOLDER
+     yes | aws s3 cp $DRIVER_LOG_LOCATION $TARGET_FOLDER --recursive
+     find $TARGET_FOLDER -name '*.gz' -exec gunzip {} +
+     echo "cat $TARGET_FOLDER/stdout"
+}
+
+
+download_logs_prod() {
      export VIRTUAL_CLUSTER_ID=f7ra6rarkleapxysng71scsl1
      LOG_BASE_URI="s3://pgy-luigi-emr-prod/logs/eks/joblogs/$1"
      PODS_LOG_LOCATION="$LOG_BASE_URI/$VIRTUAL_CLUSTER_ID/jobs/$2/"
@@ -14,10 +28,9 @@ function download_logs {
      DRIVER_LOG_LOCATION="${PODS_LOG_LOCATION}containers/spark-$2/${DRIVER_POD}/"
      TARGET_FOLDER="/tmp/spark_logs/$2"
      mkdir -p $TARGET_FOLDER
-     aws s3 cp $DRIVER_LOG_LOCATION $TARGET_FOLDER --recursive
-     find $TARGET_FOLDER -name *.gz -exec gunzip {} +
+     yes | aws s3 cp $DRIVER_LOG_LOCATION $TARGET_FOLDER --recursive && \
+     find $TARGET_FOLDER -name '*.gz' -exec gunzip {} + && \
      echo "cat $TARGET_FOLDER/stdout"
-     echo "cat $TARGET_FOLDER/stderr"
 }
 
 
@@ -59,4 +72,18 @@ merge() {
     git reset --hard "origin/$BASE_BRANCH" && \
     git merge --ff-only "$CURR_BRANCH" && \
     git push
+}
+
+black_and_isort() {
+    black -l 120 "$@" &&  \
+    isort -l 120 --profile black "$@"
+}
+
+
+create_pyenv() {
+    venv_name=$(basename $PWD)
+    echo "Creating venv with name $venv_name"
+    sleep 2
+    pyenv virtualenv 3.10.12 $venv_name
+    pyenv local $venv_name
 }
